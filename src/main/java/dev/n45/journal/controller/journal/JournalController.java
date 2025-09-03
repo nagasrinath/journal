6b72ea9a6a28186390cfdf278b31a041e89d1b5c
Naga Srinath <73869693+nagasrinath@users.noreply.github.com>
2025-09-03 19:57:57 +0530

    update

--- src/main/java/dev/n45/journal/controller/journal/JournalController.java
+++ src/main/java/dev/n45/journal/controller/journal/JournalController.java
@@ -5,6 +5,8 @@ import dev.n45.journal.entity.journal.Journal;
 import dev.n45.journal.service.journal.JournalService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -14,8 +16,11 @@ public class JournalController {
   @Autowired private JournalService journalService;
 
   @PostMapping("/journals")
-  public Journal createJournal(@RequestBody JournalRequest journalRequest, @RequestHeader(name = "X-User-Id") String userId) {
-    return journalService.createJournal(journalRequest, userId);
+  public ResponseEntity<Journal> createJournal(@RequestBody JournalRequest journalRequest, @RequestHeader(name = "X-User-Id") String userId) {
+
+      var response = journalService.createJournal(journalRequest, userId);
+
+    return new ResponseEntity<>(response, HttpStatus.CREATED);
   }
 
   @GetMapping("/journals/{id}")
