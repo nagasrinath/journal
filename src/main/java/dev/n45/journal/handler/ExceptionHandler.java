4c75e288792cb6496fca6b79b21722574f3cbd72
nagasrinath <nagasrinath@proton.me>
2025-09-04 05:41:23 +0530

    update

--- src/main/java/dev/n45/journal/handler/ExceptionHandler.java
+++ src/main/java/dev/n45/journal/handler/ExceptionHandler.java
@@ -1,4 +1,12 @@
 /* (C) 2025 Naga Srinath */
 package dev.n45.journal.handler;
 
-public class ExceptionHandler {}
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+  public static final String INVALID_REQUEST_BODY = "INVALID_REQUEST_BODY";
+  public static final String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+}
